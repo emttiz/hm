1f2104deb50e7413dd7d9edbb92aaa73f247bfdb
emttiz <meder06jylkybaev04@gmail.com>
2022-10-26 17:56:56 +0600

    homework 5

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from aiogram.utils import executor
 from config import dp
 import logging
-from handlers import client, callback, extra, fsmAdminMentor
+from handlers import client, callback, extra, fsmAdminMentor, notifications
 from database.bot_db import sql_create
+import asyncio
 
-async def on_startup( ):
+async def on_startup(_):
+    asyncio.create_task(notifications.schedule())
     sql_create()
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 fsmAdminMentor.register_handlers_fsm_anketa(dp)
+notifications.register_handlers_notification(dp)
 extra.register_handlers_extra(dp)
 
 from config import bot, dp
@@ -17,4 +20,4 @@ import logging
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup())
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
